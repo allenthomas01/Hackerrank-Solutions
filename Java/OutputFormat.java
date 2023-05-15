7596e9750d131112bb3dd32cacd028a732c73ada
Allen <allenthomas204@gmail.com>
2023-05-15 16:36:06 +0530

    Update OutputFormat.java

--- Java/OutputFormat.java
+++ Java/OutputFormat.java
@@ -6,8 +6,16 @@ public class OutputFormat{
     public static void main(String[] args) {
         /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        int num = sc.nextInt();
-        System.out.printf("%-15s %03d",str,num);
+        System.out.println("================================");
+        for(int i=0;i<3;i++){
+            String str = sc.next(); //takes string till white space. i.e takes a single word
+            int num = sc.nextInt();
+            System.out.printf("%-15s%03d\n",str,num); 
+            //%-15s left aligns the string to 15 characters.
+            //%15s right aligns "    "   "    "  ...
+        }
+        System.out.println("================================");
+        
+        ;
     }
-}   
+} 
